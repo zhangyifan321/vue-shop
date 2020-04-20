4ff952c5fd4da478612390481f3192e2eda5ea0e
zhangyifan <1347196386@qq.com>
2020-04-20 15:15:13 +0800

    master

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Mine from '../views/Mine.vue'
 Vue.use(VueRouter)
 
   const routes = [
@@ -17,6 +17,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/mine',
+    name: 'Mine',
+    component: Mine
   }
 ]
 
